46d6d4f02c8a21e835fe91343db219d11c389a4c
aisu2005 <aisuk522005@gmail.com>
2026-03-24 14:28:19 +0530

    completed

---
+++ pallindrome.sh
@@ -0,0 +1,20 @@
+if [ $# -ne 1 ]
+then
+	echo "Syntax is <$0> <number>"
+	exit 1
+fi
+no=$1
+rev=0
+while [ $no -ne 0 ]
+do
+	s=`expr $no % 10`
+	rev=`expr $rev \* 10 + $s`
+	no=`expr $no / 10`
+done
+if [ $1 -eq $rev ]
+then
+	echo "Number is pallindrome"
+else
+	echo "Not a pallindrome number"
+fi
+
