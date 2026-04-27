fcfc3ab4be1c01833a6dd4e9e051288ac98383f5
aisu2005 <aisuk522005@gmail.com>
2026-04-27 11:47:55 +0530

    completed

--- menu.sh
+++ menu.sh
@@ -1,52 +1,61 @@
-if [ $# -eq 0 ]
-then
-echo "syntax is <$0> <filename>"
-exit 1
-fi
-if [ ! -e $1 ]
-then
-echo "file doesn't exist"
-exit 1
-fi
-echo "1.ordinary file"
-echo "2.directory file"
-echo "3.readable to ugo"
-echo "4.executable to ugo"
-echo "enter your choice"
+
+echo "MENU"
+echo "1. Ordinary files"
+echo "2. Directory files"
+echo "3. Files readable to UGO"
+echo "4.Files excutable to UGO"
+echo "Enter your choice:"
 read ch
+
 case $ch in
 1)
-if [ -f $1 ]
+for f in *
+do
+if [ -f $f ]
 then
-echo "it is a ordinary file"
-else
-echo "not an ordinary file"
+echo $f
 fi
+done
 ;;
 2)
-if [ -d $1 ]
+for f in *
+do
+if [ -d $f ]
 then
-echo "it is a directory"
-else
-echo "not a directory"
+echo $f
 fi
+done
 ;;
 3)
-if [ -r $1 ]
+for f in *
+do
+if [ -f $f ]
 then
-echo "it is readable"
-else
-echo "not readable"
+u=`ls -l $f | cut -c 2-2`
+g=`ls -l $f | cut -c 5-5`
+o=`ls -l $f | cut -c 8-8`
+if [ $u = "r" -a $g = "r" -a $o = "r" ]
+then
+echo $f
 fi
+fi
+done
 ;;
 4)
-if [ -x $1 ]
+for f in *
+do
+if [ -f $f ]
 then
-echo "it is executable"
-else
-echo "not executable"
+u=`ls -l $f | cut -c 4-4`
+g=`ls -l $f | cut -c 7-7`
+o=`ls -l $f | cut -c 10-10`
+if [ $u = "x" -a $g = "x" -a $o = "x" ]
+then
+echo $f
 fi
+fi
+done
 ;;
-*)
-echo "invlid choice"
-esac 
+*)echo "invalid choice"
+;;
+esac
